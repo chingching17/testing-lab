251ae68ec4bf410f6ae832108b8d9511762073eb
chingching17 <73006493+chingching17@users.noreply.github.com>
2024-04-02 14:16:26 +0000

    done test create/ findAll

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -1,8 +1,23 @@
 import { describe, expect, test } from 'vitest'
 import { serverOf } from '../src/server'
+import * as TodoRepo from '../src/repo/todo'
+import { Todo, TodoBody } from '../src/types/todo'
 
 describe('Server Testing', () => {
   const server = serverOf()
+
+  test('Create a new input, When send a Get request to /api/v1/todos, It should tell it is written into database', async () => {
+    const newBody:Todo = {
+      id: "660bf6473f7e41a7451a5105",
+      name: "difjo",
+      description: "jofeioj",
+      status: false
+    };
+
+    const createdTodo = TodoRepo.createTodo(newBody);
+    const returnTodo = TodoRepo.findAllTodos()
+    expect(returnTodo).toMatchObject(createdTodo)
+  })
 
   test('When send a GET request to /ping, it should return status code 200', async () => {
     // act: send a GET request to /ping
