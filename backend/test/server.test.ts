462e1ff63f2115cea58b342548a8714d60e9ce56
chingching17 <73006493+chingching17@users.noreply.github.com>
2024-04-03 15:05:57 +0000

    add test updateTodoStatus

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -2,10 +2,12 @@ import { describe, expect, test } from 'vitest'
 import { serverOf } from '../src/server'
 import * as TodoRepo from '../src/repo/todo'
 import { Todo, TodoBody } from '../src/types/todo'
+import { ModifyResult } from 'mongoose'
+import todo from '../src/models/todo'
 
 describe('Server Testing', () => {
   const server = serverOf()
-
+  // first add
   test('Create a new input, Use findAllTodos to get items, It should tell createTodo and findAllTodos are correct', async () => {
     const newBody:Todo = {
       id: "660bf6473f7e41a7451a5105",
@@ -19,6 +21,7 @@ describe('Server Testing', () => {
     expect(returnTodo).toMatchObject(createdTodo)
   })
 
+  // second add
   test('Create a new input, Use deleteTodoById to delete items, It should tell createTodo and deleteTodoById are correct', async () => {
     const newBody:Todo = {
       id: "660bf6473f7e41a7451a5105",
@@ -30,6 +33,29 @@ describe('Server Testing', () => {
     const createdTodo = TodoRepo.createTodo(newBody);
     const delTodo = TodoRepo.deleteTodoById("660bf6473f7e41a7451a5105")
     expect(createdTodo).toMatchObject(delTodo)
+  })
+
+  // third add
+  test('Create a new input, Use updateTodoById to update items, It should tell createTodo and updateTodoById are correct', async () => {
+    const newBody:Todo = {
+      id: "660bf6473f7e41a7451a5105",
+      name: "difjo",
+      description: "jofeioj",
+      status: false
+    };
+
+    const ansBody:Todo = {
+      id: "660bf6473f7e41a7451a5105",
+      name: "difjo",
+      description: "doisu",
+      status: true
+    };
+
+    const createdTodo = TodoRepo.createTodo(newBody);
+    const updateTodo = TodoRepo.updateTodoById("660bf6473f7e41a7451a5105",ansBody)
+    // console.log(updateTodo)
+    // console.log(updateTodo._update)
+    expect(ansBody).toMatchObject(updateTodo._update)
   })
 
   test('When send a GET request to /ping, it should return status code 200', async () => {
